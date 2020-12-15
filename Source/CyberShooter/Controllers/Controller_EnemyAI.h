02ea5079c320c604545f9903b821c1daefe2b941
thiles03 <thiles03@gmail.com>
2020-12-15 12:06:18 +0000

    Enemy AI

---
+++ Source/CyberShooter/Controllers/Controller_EnemyAI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Controller_EnemyAI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CYBERSHOOTER_API AController_EnemyAI : public AAIController
+{
+	GENERATED_BODY()
+	
+};
