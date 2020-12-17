0cd4df0e3d8cdeb3454e0b53bd5c84b6e8170384
thiles03 <thiles03@gmail.com>
2020-12-17 23:24:03 +0000

    Enemy AI

--- Source/CyberShooter/AI/BTTask_ClearBlackboardValue.cpp
+++ Source/CyberShooter/AI/BTTask_ClearBlackboardValue.cpp
@@ -1,2 +1,17 @@
-#include "AI/BTTask_ClearBlackboardValue.h"
+#include "CyberShooter/AI/BTTask_ClearBlackboardValue.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
+
+UBTTask_ClearBlackboardValue::UBTTask_ClearBlackboardValue() 
+{
+    NodeName = TEXT("Clear Blackboard Value");
+}
+
+EBTNodeResult::Type UBTTask_ClearBlackboardValue::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) 
+{
+    Super::ExecuteTask(OwnerComp, NodeMemory);
+
+    OwnerComp.GetBlackboardComponent()->ClearValue(GetSelectedBlackboardKey());
+
+    return EBTNodeResult::Succeeded;
+}
--- Source/CyberShooter/AI/BTTask_ClearBlackboardValue.h
+++ Source/CyberShooter/AI/BTTask_ClearBlackboardValue.h
@@ -8,5 +8,11 @@ UCLASS()
 class CYBERSHOOTER_API UBTTask_ClearBlackboardValue : public UBTTask_BlackboardBase
 {
 	GENERATED_BODY()
+
+public:
+	UBTTask_ClearBlackboardValue();
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory);
 	
 };
