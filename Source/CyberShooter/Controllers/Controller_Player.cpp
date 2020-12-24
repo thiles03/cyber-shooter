4415eab373159f3093efb707f307765c687691c7
thiles03 <thiles03@gmail.com>
2020-12-24 21:42:47 +0000

    Lose screen

--- Source/CyberShooter/Controllers/Controller_Player.cpp
+++ Source/CyberShooter/Controllers/Controller_Player.cpp
@@ -1,11 +1,18 @@
 #include "Controller_Player.h"
 #include "TimerManager.h"
+#include "Blueprint/UserWidget.h"
 
 void AController_Player::GameHasEnded(class AActor *EndGameFocus, bool bIsWinner)
 {
     Super::GameHasEnded(EndGameFocus, bIsWinner);
 
     GetWorldTimerManager().SetTimer(RestartTimer, this, &APlayerController::RestartLevel, RestartDelay);
+
+    UUserWidget *LoseScreen = CreateWidget(this, LoseScreenClass);
+    if (LoseScreen)
+    {
+        LoseScreen->AddToViewport();
+    }
 }
 
 void AController_Player::SetPlayerEnabledState(bool IsEnabled)
